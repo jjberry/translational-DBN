da5fa539cb69a58fabd009a45c2ed53d46d76e14
Jeff Berry <jjberry@github.com>
2013-01-22 12:55:20 +0100

    added some utility functions to help with analysis of results

--- autoencoder.py
+++ autoencoder.py
@@ -2,7 +2,9 @@ import numpy as np
 import deepnet
 import backprop
 import cPickle as pickle
-
+import scipy.io
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
 def demo_autoencoder():
     #load and norm the data
@@ -11,21 +13,17 @@ def demo_autoencoder():
     data /= 255.0
     #set up and train the initial deepnet
     dnn = deepnet.DeepNet([data.shape[1], data.shape[1], data.shape[1], 
-        data.shape[1]*2], ['gaussian','sigmoid','sigmoid','sigmoid'])
-    dnn.train(data, [5, 5, 5], 0.0025)
+        42], ['sigmoid','sigmoid','sigmoid','sigmoid'])
+    dnn.train(data, [225, 75, 75], 0.0025)
     #save the trained deepnet
     pickle.dump(dnn, file('pretrained.pkl','wb'))
     #unroll the deepnet into an autoencoder
     autoenc = unroll_network(dnn.network)
-    #print out the sizes of the autoenc layers
-    for i in range(len(autoenc)):
-        print autoenc[i].W.shape
-        print autoenc[i].hbias.shape
-    #fine-tune with backprop
+    ##fine-tune with backprop
     mlp = backprop.NeuralNet(network=autoenc)
-    trained = mlp.train(mlp.network, data, data, max_iter=10, 
+    trained = mlp.train(mlp.network, data, data, max_iter=30, 
             validErrFunc='reconstruction', targetCost='linSquaredErr')
-    #save
+    ##save
     pickle.dump(trained, file('network.pkl','wb'))
 
 def unroll_network(network):
@@ -45,6 +43,45 @@ def unroll_network(network):
     encoder.extend(decoder)
     return encoder
 
+def save_net_as_mat(pickled_net):
+    '''
+    Takes the network pickle file saved in demo_autoencoder and saves it as a .mat
+    file for use with matlab
+    '''
+    network = pickle.load(file(pickled_net,'rb'))
+    mdic = {}
+    for i in range(len(network)/2):
+        mdic['W%d'%(i+1)] = network[i].W.as_numpy_array()
+        mdic['b%d'%(i+1)] = network[i].hbias.as_numpy_array()
+        mdic['hidtype%d'%(i+1)] = network[i].hidtype
+    scipy.io.savemat('network.mat', mdic)
+
+def visualize_results(netfile, datafile):
+    network = pickle.load(file(netfile, 'rb'))
+    #network = unroll_network(dnn.network)
+    data = np.load(datafile)
+    data = np.asarray(data, dtype='float32')
+    data /= 255.0
+    mlp = backprop.NeuralNet(network=network)
+    recon = mlp.run_through_network(data, network)
+    inds = np.arange(recon.shape[0])
+    np.random.shuffle(inds)
+    for i in range(10):
+        dim = int(np.sqrt(data.shape[1]))
+        orig = data[inds[i]].reshape((dim,dim))
+        rec = recon[inds[i]].reshape((dim,dim))
+        plt.figure(i)
+        ax = plt.subplot(211)
+        plt.imshow(orig, cmap=cm.gray)
+        ax.set_yticks([])
+        ax.set_xticks([])
+        ax = plt.subplot(212)
+        plt.imshow(rec, cmap=cm.gray)
+        ax.set_yticks([])
+        ax.set_xticks([])
+        plt.savefig('img_%d.jpg'%(inds[i]))
+
 if __name__ == "__main__":
     demo_autoencoder()
+    visualize_results('network.pkl','scaled_images.npy')
 
